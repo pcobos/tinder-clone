623c5b18daf919788902165f216b0f2dbe53e601
Pedro Cobos <pedro.cobos@live.com>
2020-06-12 21:02:56 -0500

    updated browse view to show the user photo

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "> Clearing Database..."
+Account.destroy_all
 
+puts "> Creating users..."
 
 Account.create([
   {first_name: "Pedro", last_name: "Cobos", username: "pcobos", email: "peter@cobos.com", password: "123456"},
@@ -13,3 +16,5 @@ Account.create([
   {first_name: "Andy", last_name: "Jones", username: "ajones", email: "andy@user.com", password: "123456"},
   {first_name: "Alex", last_name: "Baby", username: "ssmith", email: "alex@user.com", password: "123456"},
 ])
+
+puts "> Done!"
