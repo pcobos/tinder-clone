9ac77997b8426b156ca8dd30d7f72dccc4f1d3a5
Pedro Cobos <pedro.cobos@live.com>
2020-08-18 17:49:39 -0500

    added approve and decline routes for the browser controller

--- app/javascript/includes/slides.js
+++ app/javascript/includes/slides.js
@@ -1,28 +1,31 @@
-$(function(){
-  var $activeSlide = $('#slides .slide:first-child');
+$(function () {
+  var $activeSlide = $("#slides .slide:first-child");
 
   // show first slide
   $activeSlide.addClass("showing");
 
-  $("#decline").on("click", function(){
-    goToSlide('decline');
+  $("#decline").on("click", function () {
+    goToSlide("decline");
   });
-  
-  $("#approve").on("click", function(){
-    goToSlide('approve');
+
+  $("#approve").on("click", function () {
+    var user_id = $activeSlide.data("id");
+
+    $.ajax({
+      url: "/approve/",
+    });
+    goToSlide("approve");
   });
 
   // in this function we want to always go to the next user no matter if the user clicks on approve or decline.
-  function goToSlide(action){
+  function goToSlide(action) {
     $activeSlide.removeClass("showing");
-    $activeSlide = $activeSlide.next(".slide")
+    $activeSlide = $activeSlide.next(".slide");
 
-    if(action == "approve"){
+    if (action == "approve") {
     } else {
-      
     }
-    
+
     $activeSlide.addClass("showing");
-  
   }
 });
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "/browse", to: "browse#browse", as: :browse
+  post "/approve/:id", to: "browse#approve", as: :approve_user
+  post "/decline/:id", to: "browse#decline", as: :decline_user
 
   root to: "public#home"
 end 
