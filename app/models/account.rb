5c4b2195a7b52e6c02b01f6f23c2c6259d10d827
Pedro Cobos <pedro.cobos@live.com>
2020-08-24 15:40:22 -0500

    added matches method to Account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -5,5 +5,19 @@ class Account < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many_attached :images
+
+  def matches
+
+    liked_account_ids = Like.where(account_id: self.id).map(&:liked_account_ids)
+    puts liked_account_ids
+
+    likes_me_account_ids = Like.where(liked_account_ids: self.id).map(&:account_id)
+    puts likes_me_account_ids
+
+    matched_ids = liked_account_ids.select{ |id|  likes_me_account_ids.include?(id)}
+    puts matched_ids
+
+  end
+  
   
 end
