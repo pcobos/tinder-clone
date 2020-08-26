bfc385c225882f48a29be048b08de7adfe176b54
Pedro Cobos <pedro.cobos@live.com>
2020-08-26 15:26:46 -0500

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require "open-uri"
+
+
 puts "> Clearing Database..."
 Account.destroy_all
 
 puts "> Creating users..."
 
-Account.create([
-  {first_name: "Pedro", last_name: "Cobos", username: "pcobos", email: "peter@cobos.com", password: "123456"},
-  {first_name: "Sarah", last_name: "Smith", username: "ssmith", email: "sarah@user.com", password: "123456"},
-  {first_name: "Andy", last_name: "Jones", username: "ajones", email: "andy@user.com", password: "123456"},
-  {first_name: "Alex", last_name: "Baby", username: "ssmith", email: "alex@user.com", password: "123456"},
-])
+# FILE = File.open('app/assets/images/portrait.jpg')
+
+15.times do 
+  Account.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    username: Faker::Games::Witcher.monster,
+    email: Faker::Internet.email,
+    password: "123456",
+  )
+  # account.images.attach(io: FILE, filename: 'person.jpg', content_type: 'image/jpg')
+end
 
 puts "> Done!"
